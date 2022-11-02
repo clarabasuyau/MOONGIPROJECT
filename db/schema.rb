9c59850b28bb2b48b5b042b98779f3e3eb02d910
clara basuyau <clarabasuyau@gmail.com>
2022-11-02 18:30:46 +0100

    create message model for the form in the contact page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_145657) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_173020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_145657) do
     t.string "name"
     t.text "description"
     t.string "gramme"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sales", force: :cascade do |t|
