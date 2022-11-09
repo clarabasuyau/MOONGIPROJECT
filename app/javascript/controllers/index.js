f6378fe4c7c1f46d71a7c846bd7f32a0f8ac9f21
clara basuyau <clarabasuyau@gmail.com>
2022-11-09 18:13:04 +0100

    try to make nice carousel in home page

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import SlickController from "./slick_controller"
+application.register("slick", SlickController)
