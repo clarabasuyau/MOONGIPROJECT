4fa7ac9cbb6aa6f22c78776b806628f5388b1b8c
clara basuyau <clarabasuyau@gmail.com>
2022-11-17 15:18:25 +0100

    start to add stuff to send mail

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,49 @@ class Message < ApplicationRecord
 
   # after_create :send_mail
 
-  # def send_email
+  # def send_em
+  # belongs_to :donvert, optional: true
+  # validates :content, :sender_full_name, presence: true, length: { minimum: 4 }
+  # validates :sender_email, presence: true, format: { with: /\A[^@\s]+@[^@^.\s]+\.\w+\z/ }
+  # validates :message_type, inclusion: { in: ['interet-donvert', 'message-membres', 'message-agglo', 'message-to-referent'] }, presence: true
+
+  # # rajouter ici le after create send email to that dude
+  # after_create :send_interest_don_email, if: :message_type_is_interet_donvert?
+  # after_create :send_message_to_members_email, if: :message_type_is_message_membres?
+  # after_create :send_message_to_agglo_email, if: :message_type_is_message_agglo?
+  # after_create :send_message_to_referent_email, if: :message_type_is_message_to_referent?
+
+  # private
+
+  # def send_interest_don_email
+  #   MessageMailer.with(message: self).interest_don.deliver_now
+  # end
+
+  # def message_type_is_interet_donvert?
+  #   self.message_type == 'interet-donvert'
+  # end
+
+  # def send_message_to_members_email
+  #   MessageMailer.with(message: self).message_to_members.deliver_now
+  # end
+
+  # def message_type_is_message_membres?
+  #   self.message_type == 'message-membres'
+  # end
+
+  # def send_message_to_agglo_email
+  #   MessageMailer.with(message: self).message_to_agglo.deliver_now
+  # end
+
+  # def message_type_is_message_agglo?
+  #   self.message_type == 'message-agglo'
+  # end
+
+  # def send_message_to_referent_email
+  #   MessageMailer.with(message: self).message_to_referent.deliver_now
+  # end
+
+  # def message_type_is_message_to_referent?
+  #   self.message_type == 'message-to-referent'
   # end
 end
