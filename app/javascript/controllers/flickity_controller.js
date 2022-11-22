bf3181f36f20224e1265feea9c7e15d0f92d7bb1
paul lahana <paul.lahana@gmail.com>
2022-11-22 12:08:10 +0100

    fix flickity config, but where should we put the flickity slider now ?

--- app/javascript/controllers/flickity_controller.js
+++ app/javascript/controllers/flickity_controller.js
@@ -1,51 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
-import Flickity  from "flickity/js/index";
-
-// export default class extends Controller {
-//   connect() {
-//     new Flickity( this.element, {
-//       // autoPlay: 1500,
-//       draggable: true,
-//       freeScroll: true,
-//       wrapAround: true,
-//       contain: true,
-//       prevNextButtons: false,
-//       pageDots: false,
-//       setGallerySize: false
-//     });
-//   }
-// }
-
-// const initFlickity = () => {
-//   if (document.querySelector('.carousel-moongi')) {
-//     const elem = document.querySelector('.carousel-moongi');
-
-//     const flkty = new Flickity( elem, {
-//       cellAlign: 'left',
-//       wrapAround: true,
-//       draggable: true,
-//       lazyLoad: true,
-//       freeScroll: true,
-//       pageDots: false,
-//       cellSelector: '.carousel-cell'
-//     });
-//   }
-// }
-
-// export {initFlickity} ;
-// Import the Flickity library
 import Flickity from "flickity";
 
-// Wait for content load
-window.addEventListener("load", function() {
-
-// Initialise Flickity
-const flkty = new Flickity(".carousel", {
-        //Flickity options
-});
-
-// Due to the implementation of Flickity, a resize is required.
-// This can be accomplished by manually resizing the browser viewport
-// or do it here with JS see: https://github.com/metafizzy/flickity/issues/205
-flkty.resize();
-});
+export default class extends Controller {
+  connect() {
+    new Flickity( this.element, {
+      // autoPlay: 1500,
+      draggable: true,
+      freeScroll: true,
+      wrapAround: true,
+      contain: true,
+      prevNextButtons: false,
+      pageDots: false,
+      setGallerySize: false
+    });
+  }
+}
