e7582570e091e82b81ac4f46c24174d1641f078c
clara basuyau <clarabasuyau@gmail.com>
2022-11-23 11:05:52 +0100

    add mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  host = 'contact@moongi.fr' #replace with your own url
+  host = 'moongi.fr' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
